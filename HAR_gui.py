fe2cdfe4f215addb99a3f43589afa16b353c8e18
ShawnXuan <xiexuanx2@gmail.com>
2017-09-19 22:35:31 +0800

    open distance sensor

--- HAR_gui.py
+++ HAR_gui.py
@@ -15,9 +15,9 @@ class Application(Frame):
         t = time.localtime()
         str_t = time.strftime("%Y%m%d_%H%M%S", t)
 
-        #self.dis_thread = KDM_JG200m(cout=False)
-        #self.dis_thread.filename = str_t + "_Distance.csv"
-        #self.dis_thread.start()
+        self.dis_thread = KDM_JG200m(cout=False)
+        self.dis_thread.filename = str_t + "_Distance.csv"
+        self.dis_thread.start()
         
         self.imu_thread = IMU_Data(cout=False)
         self.imu_thread.str_t = str_t
@@ -25,17 +25,17 @@ class Application(Frame):
         print("Start recording on !",str_t)
 
     def stop(self):
-        #self.dis_thread.stop()
-        #self.dis_thread.join()
+        self.dis_thread.stop()
+        self.dis_thread.join()
         self.imu_thread.delete_file = False
         self.imu_thread.stop()
         self.imu_thread.join()
         print("Stop recording!")
 
     def abort(self):
-        #self.dis_thread.filename = None
-        #self.dis_thread.stop()
-        #self.dis_thread.join()        
+        self.dis_thread.filename = None
+        self.dis_thread.stop()
+        self.dis_thread.join()        
         self.imu_thread.delete_file = True
         self.imu_thread.stop()
         self.imu_thread.join()        
